998a0586d2515b976fec66c7be59871513a12232
Ruud Diterwich <ruud@diterwich.com>
2012-10-27 14:00:25 +0200

    added cassie dependency

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,6 +7,9 @@ object Application extends Controller {
 //  def index = Action {
 //    Ok(views.html.index("Your new application is ready."))
 //  }
+  
+//  val cluster = new Cluster("host1,host2", OstrichStatsReceiver)
+  
   def index = Action {
 	  Ok("Hello world")
 	}
--- project/Project.scala
+++ project/Project.scala
@@ -7,12 +7,18 @@ object ApplicationBuild extends Build {
   val appName         = "bigsaas"
   val appVersion      = "1.0.1-SNAPSHOT"
  
-  val appDependencies = Nil
- 
   val scalaVersion = "2.9.2"
   
+  val twttr = "Twitter's Repository" at "http://maven.twttr.com/"
+  
+  val cassie = "com.twitter" % "cassie-core" % "0.23.0"
+
+  val appDependencies = Seq(cassie)
+    
   val main = PlayProject(
     appName, appVersion, appDependencies, mainLang = SCALA
-  ) 
+  ).settings(
+      resolvers += twttr
+  )
  
 }
